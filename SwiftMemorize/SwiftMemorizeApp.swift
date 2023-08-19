3305ecaae24242eed2ad2e55a97d1da0d1aeea1a
José Lagos <joseantoniolagosg@gmail.com>
2023-08-19 19:33:38 -0300

    Initial Commit

---
+++ SwiftMemorize/SwiftMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMemorizeApp.swift
+//  SwiftMemorize
+//
+//  Created by José Lagos Guevara on 19-08-23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
